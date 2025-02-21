5b2495e427aa50f44d5b0a3fa28ee53f3291770d
zaineel <127276018+zaineel@users.noreply.github.com>
2025-02-20 20:28:34 -0600

    first commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,20 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from 'next/navigation';
-import DashboardNav from './nav';
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import DashboardNav from "./nav";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = auth();
-  
+  const { userId } = await auth();
+
   if (!userId) {
-    redirect('/sign-in');
+    redirect("/sign-in");
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className='min-h-screen bg-gray-50'>
       <DashboardNav />
       {children}
     </div>
